e67e848b1e86921b22ab64e70ba0da7030099fe0
Andrés <lopez.andresrigoberto@gmail.com>
2022-12-25 18:43:59 -0600

    add categories

---
+++ src/data/categories.js
@@ -0,0 +1,34 @@
+const categories = [
+  {
+    id: 1,
+    title: "Hats",
+    imageUrl:
+      "https://i.pinimg.com/originals/19/55/38/195538aba7a902320e22ff785df8cdcb.jpg",
+  },
+  {
+    id: 2,
+    title: "Jacket",
+    imageUrl:
+      "https://ups.aopcdn.com/s24610/goods/19271/267ua0976bec9100443f8bc1d402a16fe962.jpg?x-oss-process=image/resize,limit_0,m_pad,w_600,h_800/format,webp/interlace,1/quality,q_80",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    imageUrl:
+    "https://cdn.pixabay.com/photo/2016/11/19/18/06/feet-1840619_960_720.jpg",
+  },
+  {
+    id: 4,
+    title: "Mens",
+    imageUrl:
+      "https://media.gq.com/photos/5a516a145fcfbf58a755ba59/master/w_1920%2Cc_limit/180106_LFWMFW18-19_DR12-6368.jpg",
+  },
+  {
+    id: 5,
+    title: "Womens",
+    imageUrl:
+      "https://outfittrends.b-cdn.net/wp-content/uploads/2021/07/trendy-smart-casual-attire.jpg.webp",
+  },
+  ,
+];
+export default categories;
