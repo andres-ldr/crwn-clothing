7f2b472e793706fa0e6d9a77f7a8f9d454edc070
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-01 21:27:04 -0600

    add routes and main styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/routes/home/home.component";
+import Navigation from "./components/navigation/navigation.component";
+import Authentication from "./components/routes/authentication/Authentication.component";
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Home />}></Route>
+        <Route path="auth" element={<Authentication />}></Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
