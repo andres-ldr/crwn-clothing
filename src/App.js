0dfdeb1f9aa64fe754ea2802f021ddf8439f2c22
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-03 19:45:21 -0600

    add shop component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
 import Navigation from "./components/navigation/navigation.component";
 import Authentication from "./components/routes/authentication/Authentication.component";
+import Shop from "./components/routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />}></Route>
-        <Route path="shop" element={<Home />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
         <Route path="auth" element={<Authentication />}></Route>
       </Route>
     </Routes>
