92700d898168992b4c3435b9ebed895fffb46ba3
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-04 20:43:16 -0600

    add cartContext in Cart Icon to toggle

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { CartContext } from "../../context/cart.context";
+
 import "./cart-icon.styles.scss";
 
-const CartIcon = () => {
+const CartIcon = (props) => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+  const toggleisCartOpen = () => setIsCartOpen(!isCartOpen);
+
   return (
-    <div className="cart-icon-container">
+    <div className="cart-icon-container" onClick={toggleisCartOpen}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
