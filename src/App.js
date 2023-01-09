03f8e9170bb182e4bc6d04480632fe6f601e0320
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-08 18:54:42 -0600

    [edit] route syntax

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Navigation from "./components/navigation/navigation.component";
 import Authentication from "./components/routes/authentication/Authentication.component";
 import Shop from "./components/routes/shop/shop.component";
 import CheckOut from "./components/routes/checkout/checkout.component";
+import CategoriesPreview from "./components/routes/categories-preview/categories-preview.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />}></Route>
-        <Route path="shop" element={<Shop />}></Route>
+        <Route path="shop/*" element={<Shop />}></Route>
         <Route path="auth" element={<Authentication />}></Route>
         <Route path="checkout" element={<CheckOut/>}></Route>
       </Route>
