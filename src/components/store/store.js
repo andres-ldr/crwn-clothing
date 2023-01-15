5c139924e3e3c32839672f23d66e9407145f923c
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-14 21:44:15 -0600

    [add] redux store

---
+++ src/components/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore, compose, applyMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
+
+const middleWares = [logger];
+
+//const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [logger],
+});
