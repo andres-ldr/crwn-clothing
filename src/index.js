a91bc10d6b238f256dfa16cabf7753e4cd98ff71
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-19 20:06:07 -0600

    [add] persist gate with persistor

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { Provider } from 'react-redux';
-import { store } from './components/store/store';
+import { store, persistor } from './components/store/store';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
