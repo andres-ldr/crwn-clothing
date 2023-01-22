20bd8533447dc2b9178dcc308884b8c79f56460e
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-21 19:39:34 -0600

    [edit] replace thunk by saga

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -3,12 +3,8 @@ import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
-
-const middleWares = [
-  process.env.NODE_ENV !== 'production' && logger,
-  thunk,
-].filter(Boolean);
+import createSagaMiddleware from '@redux-saga/core';
+import { rootSaga } from './root-saga';
 
 const persistConfig = {
   key: 'root',
@@ -16,11 +12,20 @@ const persistConfig = {
   whitelist: ['cart'],
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  sagaMiddleware,
+].filter(Boolean);
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: middleWares,
 });
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
