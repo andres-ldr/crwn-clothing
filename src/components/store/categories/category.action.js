c43f81584b19cb6f1833905259f718ba48b35226
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-21 19:40:20 -0600

    [delete]  category thunk

--- src/components/store/categories/category.action.js
+++ src/components/store/categories/category.action.js
@@ -1,4 +1,3 @@
-import { getCategoriesAndDocuments } from '../../../utils/firebase.utils';
 import { createAction } from '../../../utils/reducer/reducer.utils';
 import { CATEGORIES_ACTION_TYPES } from './category.types';
 
@@ -13,13 +12,3 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 
 export const fetchCategoriesFailed = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-  try {
-    const categoryArray = await getCategoriesAndDocuments();
-    dispatch(fetchCategoriesSuccess(categoryArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-};
