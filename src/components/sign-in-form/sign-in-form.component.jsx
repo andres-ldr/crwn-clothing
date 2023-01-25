574f7b6552205695d3994e8a632a9be67cd746f3
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-24 19:11:24 -0600

    [add] dispatch sign with google saga

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-  signInAuthUserWithEmailAndPassword,
-} from "../../utils/firebase.utils";
-import FormInput from "../form-input/form-input.component";
-import Button, { BUTTON_TYPES_CLASSES } from "../button/button.component";
+import { useState } from 'react';
+import { signInAuthUserWithEmailAndPassword } from '../../utils/firebase.utils';
+import FormInput from '../form-input/form-input.component';
+import Button, { BUTTON_TYPES_CLASSES } from '../button/button.component';
 
-import "./sign-in-form.styles.scss";
+import './sign-in-form.styles.scss';
+import { useDispatch } from 'react-redux';
+import { googleSignInStart } from '../store/user/user.action';
 
 const defaultformFields = {
-  email: "",
-  password: "",
+  email: '',
+  password: '',
 };
 
 const SignInForm = () => {
+  const dispatch = useDispatch();
   const [formFields, setFormFields] = useState(defaultformFields);
   const { email, password } = formFields;
 
@@ -38,11 +37,11 @@ const SignInForm = () => {
       resetFormFields();
     } catch (err) {
       switch (err.code) {
-        case "auth/wrong-password":
-          alert("Incorrect password for email");
+        case 'auth/wrong-password':
+          alert('Incorrect password for email');
           break;
-        case "auth/user-not-found":
-          alert("No user associated with this email");
+        case 'auth/user-not-found':
+          alert('No user associated with this email');
           break;
         default:
           console.log(err);
@@ -52,38 +51,38 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    await signInWithGooglePopup();
+    dispatch(googleSignInStart());
   };
 
   return (
-    <div className="sign-up-container">
+    <div className='sign-up-container'>
       <h2>Already have an account?</h2>
       <span>Sign In with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
-          label="Email"
-          type="email"
+          label='Email'
+          type='email'
           required
           onChange={handleChange}
-          name="email"
+          name='email'
           value={email}
         />
 
         <FormInput
-          label="Password"
-          type="password"
+          label='Password'
+          type='password'
           required
           onChange={handleChange}
-          name="password"
+          name='password'
           value={password}
         />
 
-        <div className="buttons-container">
-          <Button type="submit">Sign In</Button>
+        <div className='buttons-container'>
+          <Button type='submit'>Sign In</Button>
 
           <Button
             buttonType={BUTTON_TYPES_CLASSES.google}
-            type="button"
+            type='button'
             onClick={signInWithGoogle}
           >
             Google Sign In
