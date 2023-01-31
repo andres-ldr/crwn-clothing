794ead2a73ef67ad99b9d4819dfd3abad6697557
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-30 21:51:13 -0600

    [edit] custom button spinner

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,12 +2,13 @@ import {
   BaseButton,
   GoogleSignInButton,
   InvertedButton,
-} from "./button.styles";
+  ButtonSpinner,
+} from './button.styles';
 
 export const BUTTON_TYPES_CLASSES = {
-  base: "base",
-  google: "google-sign-in",
-  inverted: "inverted",
+  base: 'base',
+  google: 'google-sign-in',
+  inverted: 'inverted',
 };
 
 const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) =>
@@ -17,10 +18,14 @@ const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) =>
     [BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
 
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return (
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>
+  );
 };
 
 export default Button;
