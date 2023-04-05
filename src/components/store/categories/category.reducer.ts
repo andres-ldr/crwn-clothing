3f635a99c29ef657013b73cbd213df66832bd461
Andrés <lopez.andresrigoberto@gmail.com>
2023-04-04 20:05:57 -0500

    [add] Category state

--- src/components/store/categories/category.reducer.ts
+++ src/components/store/categories/category.reducer.ts
@@ -1,6 +1,13 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
+import { CategoryAction } from './category.action';
 
-export const CATEGORIES_INITIAL_STATE = {
+export type CategoryState = {
+  readonly categories: Category[];
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+export const CATEGORIES_INITIAL_STATE: CategoryState = {
   categories: [],
   isLoading: false,
   error: null,
@@ -8,23 +15,21 @@ export const CATEGORIES_INITIAL_STATE = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {}
+  action = {} as CategoryAction
 ) => {
-  const { type, payload } = action;
-
-  switch (type) {
+  switch (action.type) {
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
       return { ...state, isLoading: true };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
       return {
         ...state,
-        categories: payload,
+        categories: action.payload,
         isLoading: false,
       };
     case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
       return {
         ...state,
-        error: payload,
+        error: action.payload,
         isLoading: false,
       };
     default:
